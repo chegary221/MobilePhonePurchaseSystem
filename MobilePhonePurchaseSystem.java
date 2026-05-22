0b4fb6f03e083c4c7241f4ebd88bbf3815c6bcc4
chegary221 <chegary221@gmail.com>
2026-05-22 13:12:57 +0000

    Update Java project

--- MobilePhonePurchaseSystem.java
+++ MobilePhonePurchaseSystem.java
@@ -1,6 +1,5 @@
-public class Main {
+public class MobilePhonePurchaseSystem {
         public static void main(String[] args) {
                 System.out.println("手機購買系統啟動成功！");
                     }
                     }
-}
