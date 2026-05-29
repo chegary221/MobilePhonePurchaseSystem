e8220d146a77b86a2172fe9e73f61fa41a5348c1
Tannono <91450017+Tannono@users.noreply.github.com>
2026-05-29 23:15:21 +0800

    Remove staffID field and related methods from Staff
    Removed staffID from Staff class and its constructor.

--- Staff.java
+++ Staff.java
@@ -1,18 +1,13 @@
 public class Staff extends User {
-    private String staffID;
     private int accessLevel;
 
     public Staff() {}
 
     public Staff(String userID, String name, String email, String password,
-                 String contactNum, String staffID, int accessLevel) {
+                 String contactNum, int accessLevel) {
         super(userID, name, email, password, contactNum);
-        this.staffID = staffID;
         this.accessLevel = accessLevel;
     }
-
-    public String getStaffID() { return staffID; }
-    public void setStaffID(String staffID) { this.staffID = staffID; }
 
     public int getAccessLevel() { return accessLevel; }
     public void setAccessLevel(int accessLevel) { this.accessLevel = accessLevel; }
