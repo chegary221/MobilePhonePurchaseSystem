e523de135a84526beb786f5f3f6efcbb296b13a2
sylim1368 <sylim1368@gmail.com>
2026-05-30 18:25:54 +0800

    Create Item.java

---
+++ Item.java
@@ -0,0 +1,16 @@
+public abstract class Item {
+    protected String itemID;
+    protected double price;
+
+    public Item(String itemID, double price) {
+        this.itemID = itemID;
+        this.price = price;
+    }
+
+    public String getItemID() { return itemID; }
+    public void setItemID(String itemID) { this.itemID = itemID; }
+    public double getPrice() { return price; }
+    public void setPrice(double price) { this.price = price; }
+
+    public abstract void displayItemDetails();
+}
