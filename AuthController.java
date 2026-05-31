143f83819cc4c4959b9442e95b078a639bbfe635
sylim1368 <sylim1368@gmail.com>
2026-05-31 12:01:47 +0800

    Update AuthController.java

--- AuthController.java
+++ AuthController.java
@@ -1,17 +1,17 @@
 public class AuthController {
 
-    public void authRequest(String email, String password, LoginPage page) {
+    public User authRequest(String email, String password, LoginPage page) {
         User user = getUser(email);
 
         if (user == null) {
             page.displayError("Authentication Failure: User not found.");
-            return;
+            return null;
         }
 
         // E1: Account Lockout Check
         if (user.isLocked()) {
             page.displayLockout();
-            return;
+            return null;
         }
 
         // Verify password
@@ -19,10 +19,12 @@ public class AuthController {
             // Else: Correct Password Flow
             user.resetAttempts();
             page.loadDashboard(user.getName());
+            return user;
         } else {
             // E2: Authentication Failure (Wrong Credentials)
             user.addFailAttempt();
             page.displayError("Authentication Failure: Wrong Credentials.");
+            return null;
         }
     }
 
